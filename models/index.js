5700812fbdf030cf4857f9afdafab9190ccf5139
Jake Disharoon <jakedisharoon@gmail.com>
2023-04-27 14:36:37 -0700

    models and relations

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+Comment.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "postId",
+  onDelete: "CASCADE",
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post,
+};
